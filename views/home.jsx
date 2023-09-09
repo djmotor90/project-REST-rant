f5d694508c8369052f9ffef9a06f1d519b7e1f1a
djmotor90 <98438389+djmotor90@users.noreply.github.com>
2023-09-09 10:58:31 -0500

    Adding Bootstrap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -5,15 +5,24 @@ function home () {
     return (
       <Def>
           <main>
-              <h1>HOME</h1>
-              <a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
+    <h1>HOME</h1>
+    <div>
+      <img src="/images/food1.jpg" alt="Chia Fruit Shake" />
+      <div>
+      Photo by <a href="https://unsplash.com/@goodeats_yqr?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">GoodEats YQR</a> on <a href="https://unsplash.com/photos/bMvXpKuRN3g?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+ 
+      </div>
+    </div>
+    <a href="/places">
+        <button className="btn-primary">Places Page</button>
+    </a>
+</main>
 
-          </main>
       </Def>
     )
   }
   
 
 module.exports = home
+
+
