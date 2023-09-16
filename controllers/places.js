1271525597384bba4757a32605fa7c2b66be5bbd
djmotor90 <98438389+djmotor90@users.noreply.github.com>
2023-09-15 22:41:21 -0500

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -67,14 +67,26 @@ router.get('/:id/edit', (req, res) => {
 
 // Router PUT
 router.put('/:id', (req, res) => {
-  let id = Number(req.params.id);
+  let id = Number(req.params.id)
   if (isNaN(id)) {
-      res.render('error404');
+      res.render('error404')
   }
   else if (!places[id]) {
-      res.render('error404');
+      res.render('error404')
   }
   else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+    
     // update the place with the new data
     places[id] = req.body;
 
